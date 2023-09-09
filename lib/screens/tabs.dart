84e95696635cc00412965ef79b45e21c6223e193
Mahmutjan Dawut <91298962+David007M@users.noreply.github.com>
2023-09-10 01:12:56 +0900

    filters connected to the data

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -64,7 +64,9 @@ class _TabsScreenState extends State<TabsScreen> {
     if (identifier == 'filters') {
       final result = await Navigator.of(context).push<Map<Filter, bool>>(
         MaterialPageRoute(
-          builder: (ctx) => const FiltersScreen(),
+          builder: (ctx) => FiltersScreen(
+            currentFilters: _selectedFilters,
+          ),
         ),
       );
 
@@ -94,6 +96,7 @@ class _TabsScreenState extends State<TabsScreen> {
 
     Widget activePage = CategoriesScreen(
       onToggleFavorite: _toggleMealFavoriteStatus,
+      availableMeals: availableMeals,
     );
     var activePageTitle = 'Categories';
 
